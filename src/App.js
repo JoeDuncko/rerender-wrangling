c6d0fa6a4e2c3376f8a5d975fa9c678d8c242f0e
Joe Duncko <JoeDuncko@users.noreply.github.com>
2021-11-13 20:45:34 -0500

    Start setting up examples

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import { ExampleO1 } from "./views/Example01";
+import { Navigation } from "./views/Navigation";
+
 function App() {
-  return <p>test</p>;
+  return (
+    <div>
+      <h1>React Rerender Wrangling examples</h1>
+      <Routes>
+        <Route path="/" element={<Navigation />} />
+        <Route path="/example1" element={<ExampleO1 />} />
+      </Routes>
+      <footer>
+        by <a href="https://joeduncko.com/">Joe Duncko</a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/views/Navigation.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export function Navigation() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/example1">Example 1</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
